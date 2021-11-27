4016dc61f92bf52c6b6804dccdabe4fe1e671f47
Pandunanda P. Kafah <pandukyouka@gmail.com>
2021-11-27 12:24:23 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+	int i = 0;
+    for (; i < 10000; i++)
+   {printf("\nTOLOL:TOLOL:LOL:");
+    printf("\n             A  ");
+    printf("\n L     /--------\\");
+    printf("\nLOL======       []\\");
+    printf("\n        \\          \\");
+    printf("\n         \\__________]");
+    printf("\n            I     I ");
+    printf("\n         ------------/ \n");
+    system("clear");
+    printf("\n           :LOL:TOLOL:TOLOL");
+    printf("\n             A     ");
+    printf("\n       /--------\\");
+    printf("\nLOL======       []\\");
+    printf("\n L      \\          \\");
+    printf("\n         \\__________]");
+    printf("\n            I     I ");
+    printf("\n         ------------/ \n");
+    system("clear");}    
+}
